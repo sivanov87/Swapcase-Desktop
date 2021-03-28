dd073ed034d887438464191c8380daaa14a8be00
milerius <rmscastle@gmail.com>
2021-03-28 17:53:45 +0200

    feat(simple_trading_view): remove simple comment

--- src/core/atomicdex/pages/qt.simple.trading.page.cpp
+++ src/core/atomicdex/pages/qt.simple.trading.page.cpp
@@ -21,20 +21,3 @@ namespace atomic_dex
     {
     }
 } // namespace atomic_dex
-
-/**
-    else
-    {
-        m_total_amount = total_amount;
-        emit       totalAmountChanged();
-        emit       baseAmountChanged();
-        emit       relAmountChanged();
-        t_float_50 price_f(0);
-        t_float_50 total_amount_f(safe_float(total_amount.toStdString()));
-        t_float_50 volume_f(safe_float(m_volume.toStdString()));
-        if (volume_f > 0)
-        {
-            price_f = total_amount_f / volume_f;
-        }
-        this->set_price(QString::fromStdString(utils::format_float(price_f)));
-    }*/
