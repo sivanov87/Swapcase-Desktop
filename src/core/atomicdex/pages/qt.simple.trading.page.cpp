80ab63bf0fe7dfa5b2058a3ee02e40194bd45b55
milerius <rmscastle@gmail.com>
2021-03-28 18:09:50 +0200

    feat(simple_trading_view): continue simple trading view

--- src/core/atomicdex/pages/qt.simple.trading.page.cpp
+++ src/core/atomicdex/pages/qt.simple.trading.page.cpp
@@ -21,3 +21,54 @@ namespace atomic_dex
     {
     }
 } // namespace atomic_dex
+
+namespace atomic_dex
+{
+    void
+    atomic_dex::simple_trading_page::set_from(QString from)
+    {
+        if (m_from != from)
+        {
+            m_from = std::move(from);
+            emit fromChanged();
+        }
+    }
+
+    void
+    atomic_dex::simple_trading_page::set_to(QString to)
+    {
+        if (m_to != to)
+        {
+            m_to = std::move(to);
+            emit toChanged();
+        }
+    }
+
+    void
+    atomic_dex::simple_trading_page::set_price(QString price)
+    {
+        if (m_price != price)
+        {
+            m_price = std::move(price);
+            emit priceChanged();
+        }
+    }
+
+    QString
+    simple_trading_page::get_to() const
+    {
+        return m_to;
+    }
+
+    QString
+    simple_trading_page::get_from() const
+    {
+        return m_from;
+    }
+
+    QString
+    simple_trading_page::get_price() const
+    {
+        return m_price;
+    }
+} // namespace atomic_dex
--- src/core/atomicdex/pages/qt.simple.trading.page.hpp
+++ src/core/atomicdex/pages/qt.simple.trading.page.hpp
@@ -6,15 +6,40 @@ namespace atomic_dex
     {
         Q_OBJECT
 
+        Q_PROPERTY(QString from READ get_from NOTIFY fromChanged)
+        Q_PROPERTY(QString to READ get_to NOTIFY toChanged)
+        Q_PROPERTY(QString price READ get_price NOTIFY priceChanged)
+
         //! Private member fields
         ag::ecs::system_manager& m_system_mgr;
+
+        //! Property private fields
+        QString m_from{""};
+        QString m_to{""};
+        QString m_price{""};
+
+        //! Private property setter
+        void set_from(QString from);
+        void set_to(QString to);
+        void set_price(QString price);
+
       public:
         simple_trading_page(entt::registry& registry, ag::ecs::system_manager& system_manager, QObject* parent = nullptr);
         ~simple_trading_page() final = default;
 
         //! Public override
         void update() final;
+
+        //! Property
+        [[nodiscard]] QString get_from() const;
+        [[nodiscard]] QString get_to() const;
+        [[nodiscard]] QString get_price() const;
+
+      signals:
+        void fromChanged();
+        void toChanged();
+        void priceChanged();
     };
-}
+} // namespace atomic_dex
 
 REFL_AUTO(type(atomic_dex::simple_trading_page))
